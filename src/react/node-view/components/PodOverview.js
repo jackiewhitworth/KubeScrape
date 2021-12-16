7ebc356ac297b6907ebb98b4a4bc3301423987ee
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-12-16 08:23:32 -0600

    Refactored PodOverview component

--- src/react/node-view/components/PodOverview.js
+++ src/react/node-view/components/PodOverview.js
@@ -18,38 +18,34 @@ const Detail = styled(ListItem)(({ theme }) => ({
   fontSize: '14px',
 }));
 
-const PodOverview = props => (
-  <Box
-    sx={{
-      wordWrap: 'break-word',
-      width: '350px',
-      fontSize: '1rem',
-      ':hover': {
-        filter: 'brightness(150%)',
-        cursor: 'pointer',
-      },
-    }}
-  >
-    <Paper elevation={5} sx={{ display: 'flex', alignItems: 'center', height: '200px' }}>
-      <List>
-        <Detail>
-          <span>Pod: {props.podName}</span>
-        </Detail>
-        <Detail>
-          <span>Namespace: {props.namespace}</span>
-        </Detail>
-        <Detail>
-          <span>IP: {props.ip}</span>
-        </Detail>
-        <Detail>
-          <span>Deployment: {props.deployment}</span>
-        </Detail>
-        <Detail>
-          <span>UID: {props.uid}</span>
-        </Detail>
-      </List>
-    </Paper>
-  </Box>
-);
+const PodOverview = ({ podName, namespace, ip, deployment, uid }) => {
+  const podHeadings = {
+    'Pod': podName,
+    'Namespace': namespace,
+    'IP': ip,
+    'Deployment': deployment,
+    'UID': uid
+  };
+
+  return (
+    <Box
+      sx={{
+        wordWrap: 'break-word',
+        width: '350px',
+        fontSize: '1rem',
+        ':hover': {
+          filter: 'brightness(150%)',
+          cursor: 'pointer',
+        },
+      }}
+    >
+      <Paper elevation={5} sx={{ display: 'flex', alignItems: 'center', height: '200px' }}>
+        <List>
+          {Object.entries(podHeadings).map(([heading, value]) => <Detail key={`${heading}${value}`}><span>{heading}:{value}</span></Detail>)}
+        </List>
+      </Paper>
+    </Box>
+  );
+};
 
 export default PodOverview;
